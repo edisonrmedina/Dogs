913ecdd7e9e10b3054b7dc95743c41d4f46c71cc
Edison Isaias Reinoso Medina <rein801206@siigo.com>
2023-07-09 14:32:31 -0500

    add welcome message

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,6 +9,12 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/', (req,res) => { 
+    res.status(200).json({
+        message:"Bienvenidos a mi Backend"
+    })
+ })
 router.use('/', dogRoutes)
 router.use('/', temperamentsRoutes);
 module.exports = router;
