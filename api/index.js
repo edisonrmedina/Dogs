90cdeb45c45b16f3b587cd254270ff983f484bbd
Edison Isaias Reinoso Medina <rein801206@siigo.com>
2023-07-09 14:13:44 -0500

    add new changes

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const {PORT} = process.env;
+const PORT =  process.env.PORT || 3000;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
